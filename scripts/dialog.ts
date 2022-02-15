29f5b9105bb41fc4c8d1eca1b4fd7f71a9fe7288
SungMin59 <bigmanmin1@naver.com>
2022-02-15 14:07:28 +0900

    dialog v1.0.1

--- scripts/dialog.ts
+++ scripts/dialog.ts
@@ -15,22 +15,23 @@ let scenes = jsonData['minecraft:npc_dialogue'].scenes;
 Arr2.forEach((v,index,array)=>{
     let struct = {
         "scene_tag": v[0],
-        "npc_name": {"rawtext": [{"text": "dialog." + v[1]}]},
-        "text": {"rawtext": [{"translate": "dialog." + v[2]}]},
-        "on_open_commands": [v[3]],
-        "on_close_commands": [v[4]],
+        "npc_name": {"rawtext": [{"translate": "dialog." + v[1]}]},
+        "text": {"rawtext": [{"translate": "dialog." + v[0]}]},
+        "on_open_commands": [v[2]],
+        "on_close_commands": [v[3]],
         "buttons": new Array<Object>()
     }//귀찮
-    for (let i=0;i<6;i++){
-        if (v[i+5]) struct.buttons.push({
-            "name": {"rawtext": [{"text": v[i+5]}]},
-            "commands": [`/dialogue open @e[type=lim:dialogue] @initiator ${v[i+6]}`]
+    for (let i=0;i<18;i+=3){
+        if (v[4+i]!==undefined) struct.buttons.push({
+            "name": {"rawtext": [{"text": v[4+i]}]},
+            "commands": [`/dialogue open @e[type=lim:dialogue,name=${v[6+i]}] @initiator ${v[5+i]}`]
         })
     }
-    console.log(struct);
+    console.log(v);
     scenes.push(struct);
 });
 jsonData['minecraft:npc_dialogue'].scenes = scenes;
+console.log('j');
 
 fs.writeFileSync(__dirname + `/tutorial.json`, JSON.stringify(jsonData,null,2));
 fs.writeFileSync(__dirname + `/data.txt`, textData, 'utf-8');
